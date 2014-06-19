e58d551bf4e7baca3c085b69b0f9159634f8185c
Matheus Sampaio <matheussampaio@users.noreply.github.com>
2014-06-19 18:25:14 -0300

    Update install_nodejs.sh

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-
-# Instalando Dependencias:
-sudo apt-get install -y g++ curl libssl-dev apache2-utils unzip make gcc make build-essential
-
 wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
 
 tar -xf node-v0.10.26.tar.gz
